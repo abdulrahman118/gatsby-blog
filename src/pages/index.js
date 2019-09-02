6dc46fa4d997d245cb9b89894c2acc2c59508af5
abdulrahman118 <developer.abdulrahman@gmail.com>
2019-09-02 23:49:32 +0530

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,61 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
+import styled from 'styled-components'
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const BlogLink = styled(Link)`
+text-decoration:none;
+`
+
+const BlogTitle = styled
+const IndexPage = ({data}) =>{ 
+  // console.log(data);
+  
+  return(
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title="Home" />    
+    <h1>Welcome to your new Gatsby site.</h1>
+      <h3>Total count: {data.allMarkdownRemark.totalCount}</h3>
+    {
+      data.allMarkdownRemark.edges.map(({node}) => (
+        <div key={node.id}>
+          <BlogLink to={node.fields.slug}>
+            <h4>{node.frontmatter.title} - {node.frontmatter.date}</h4>
+          </BlogLink>
+        
+        <p>{node.excerpt}</p>
+        </div>
+      ))
+    }           
   </Layout>
 )
-
+}
 export default IndexPage
+
+export const query = graphql`
+query {
+  allMarkdownRemark(sort: {fields:[frontmatter___date],order:DESC}) {
+    totalCount
+    edges {
+      node {
+        excerpt
+        fileAbsolutePath
+        html
+        id
+        frontmatter {
+          date
+          description
+          title
+        }
+        fields {
+          slug
+        }
+      }
+    }
+  }
+}
+
+`
